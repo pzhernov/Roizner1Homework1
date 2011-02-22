6c2892977b3ab1b5f0e1a5b0aa64263deffb159f
Pavel Zhernov <linux.forever.root@gmail.com>
2011-02-22 14:25:00 +0000

    some object naming functions are added

--- generator/generator.cc
+++ generator/generator.cc
@@ -20,6 +20,8 @@ void GenerateObject(vector<double>* object) {
   object->at(1) = Random();
 }
 
+void GenerateObjectD();
+
 void NameObjectA(double centre_x, double centre_y, double radius,
                  const vector<double>& object, string* name) {
   if ((object[0] - centre_x) * (object[0] - centre_x) +
@@ -33,7 +35,48 @@ void NameObjectA(double centre_x, double centre_y, double radius,
 void NameObjectB(double width, double height,
                  const vector<double>& object, string* name) {
   if (static_cast<int>(ceil(object[0] / (2 * width))) % 2 == 1) { /* / */
+    double x_zone = object[0] - (2 * width) * floor(object[0] / (2 * width));
+    double y_zone = -(height / 2) + x_zone * height / width;
+    if (object[1] > y_zone) {
+      *name = string("first");
+    } else {
+      *name = string("zero");
+    }
   } else { /* \ */
+    double x_zone = object[0] - (2 * width) * floor(object[0] / (2 * width))
+                  - width;
+    double y_zone = height / 2 - x_zone * height / width;
+    if (object[1] > y_zone) {
+      *name = string("first");
+    } else {
+      *name = string("zero");
+    }
+  }
+}
+
+void NameObjectC(int cells, const vector<double>& object, string* name) {
+  int horizontal = static_cast<int>(ceil(object[0] * cells));
+  int vertical = static_cast<int>(ceil(object[1] * cells));
+  if ((horizontal + vertical) % 2 == 0) {
+    *name = string("first");
+  } else {
+    *name = string("zero");
+  }
+}
+
+void NameObjectE(double centre_x, double centre_y, double internal_radius,
+                 doubel external_radius, const vector<double>& object,
+                 string* name) {
+  if ((object[0] - centre_x) * (object[0] - centre_x) +
+      (object[1] - centre_y) * (object[1] - centre_y) < internal_radius) {
+    *name = string("first");
+  } else {
+    if ((object[0] - centre_x) * (object[0] - centre_x) +
+        (object[1] - centre_y) * (object[1] - centre_y) > external_radius) {
+      *name = string("zero");
+    } else {
+      *name = ((rand() % 2 == 0) ? string("zero") : string("first"));
+    }
   }
 }
 
